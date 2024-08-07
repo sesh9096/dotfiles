ab7d24b6d8e0f271865d2c51a4f74554ad7f079b
sesh9096 <sesh9096@colorado.edu>
2024-08-07 11:03:25 -0600

    tweaks

--- nsl/user-overrides.js
+++ nsl/user-overrides.js
@@ -1,2 +1,4 @@
 // override delete history on exit
 user_pref("privacy.clearOnShutdown.history", false);   // [DEFAULT: true]
+user_pref("privacy.clearOnShutdown_v2.historyFormDataAndDownloads", false);   // [DEFAULT: true]
+
