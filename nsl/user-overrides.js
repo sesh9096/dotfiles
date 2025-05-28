6505cab0d6e53a2cfe18247bc3cb2a2c5ca9a7f3
sesh9096 <sesh9096@colorado.edu>
2025-05-28 15:11:47 -0600

    network changes

--- nsl/user-overrides.js
+++ nsl/user-overrides.js
@@ -1,4 +1,5 @@
 // override delete history on exit
 user_pref("privacy.clearOnShutdown.history", false);   // [DEFAULT: true]
+user_pref("privacy.clearOnShutdown_v2.browsingHistoryAndDownloads", false);   // [DEFAULT: true]
 user_pref("privacy.clearOnShutdown_v2.historyFormDataAndDownloads", false);   // [DEFAULT: true]
-
+user_pref("general.useragent.override", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Safari/537.3")
